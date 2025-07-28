0d6a7cb5ea1406dc724b0ea7b44589d128094bb8
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-07-28 15:09:07 +0530

    change direction function made

--- script.js
+++ script.js
@@ -1,24 +1,73 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const gameArena = document.getElementsByClassName('gameArena');
+    const gameArena = document.querySelector('.gameArena');
     const arenaSize = 600;
     const cellSize = 20;
     let score = 0; // score of game.
     let gameStarted = false;   // boolean flag  shows game status.
 
-    let food = {x: 300, y:200};  // {x: 15*20, y:10*20} -> as the x = 10, and y = 12. [coordinate], but to convert into pixcel we multipy by 20(cellsize)
+    let food = {x: 200, y:160};  // {x: 10*20, y:8*20} -> as the x = 10, and y = 8. [coordinate], but to convert into pixcel we multipy by 20(cellsize)
  
     // create snake as array of objects, to store the position.
 
-    let snake = [{x:160, y:200}, {x:140, y:200}, {x:120, y:200}];  // [head, body, tail]. on x -> gap of 20 is take.
+    let snake = [{x:140, y:160}, {x:120, y:160}, {x:100, y:160}];  // [head, body, tail]. on x -> gap of 20 is take.
+    
+    /*
+        consider for dx and dy -> 
+       * in case of right direction = {dx + cellsize, dy}, cells of "x" are increased in right direction = in x -> +cellsize, +ve
+       * in case of left direction = {dx - cellsize, dy}, cells of "x" are decreased in left direction = in x -> -cellsize, -ve
+       * in case of up direction = {dx, dy - cellsize}, cells of "y" are decreased in up direction = in y -> -cellSize, -ve
+       * in case of down direction = {dx, dy + cellsize}, cells of "y" are increased in down direction = in y -> +cellsize , +ve
+    */
 
- 
+    let dx = cellSize; 
+    let dy = 0;
+
+    // here "key/code" is name of key when we do keydown see in console.
+    function changeDirection(event){
+      //  console.log("key pressed", event);
+
+      //just do (or no need) a check when we go in any direction  no need to press the key again and again. 
+
+      /* also check if goingDown the can't press ArrowUp
+        if goingUp the can't press ArrowDown
+        if goingLeft the can't press ArrowRight
+        if goingRight the can't press ArrowLeft
+      */
+
+
+      const isGoingDown = dy === cellSize;
+      const isGoingUp = dy === -cellSize;
+      const isGoingLeft = dx === -cellSize;
+      const isGoingRight = dx === cellSize;
+
+      if(event.key === 'ArrowUp' && !isGoingDown){
+        dx = 0;
+        dy = -cellSize;
+      }
+      else if(event.key === 'ArrowDown' && !isGoingUp){
+        dx = 0;
+        dy = cellSize;
+      }
+      else if(event.key === 'ArrowLeft' && !isGoingRight){
+        dx = -cellSize;
+        dy = 0;
+      }
+      else if(event.key === 'ArrowRight' && !isGoingLeft) { 
+        dx = cellSize;
+        dy = 0;
+      }
+      
+        
+    }
+
+
     function drawDiv(x, y, className){
         const divElement = document.createElement('div');
         divElement.classList.add(className);  //whatever class name you want to add in div you may add int it.
         
-        // here we are getting top-left index of div =  food
+        // here we are getting top-left index of div =  food and snake
         divElement.style.top = `${y}px`; 
         divElement.style.left = `${x}px`;
 
@@ -46,6 +95,8 @@ document.addEventListener('DOMContentLoaded', () => {
             gameStarted = true; // mark the status to true
             //gameLoop();  // as the game is continuous proocess.
             drawFoodAndSnake();
+            // when key is pressed the direction is changed.
+            document.addEventListener('keydown', changeDirection);
         }
     }
 
